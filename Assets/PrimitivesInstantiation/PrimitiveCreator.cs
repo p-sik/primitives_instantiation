776b8d08962fc6b88e6040389edd44025ec3f71f
Stacionarni-PC <p3ter.sik@gmail.com>
2018-06-11 12:11:10 +0200

    Instantiation works and is only for desired types

--- Assets/PrimitivesInstantiation/PrimitiveCreator.cs
+++ Assets/PrimitivesInstantiation/PrimitiveCreator.cs
@@ -1,16 +1,44 @@
-﻿using System.Collections;
+﻿using Assets.PrimitivesInstantiation;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PrimitiveCreator : MonoBehaviour {
+public class PrimitiveCreator : MonoBehaviour
+{
+    [SerializeField] Vector3 objectScale = Vector3.one;
+    [SerializeField] ShapeType primitiveShape;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(100, 100, 600, 240), "Instantiate"))
+        {
+            PrimitiveType shape = ConvertEnum(primitiveShape);
+            PrimitiveInstantiator.Instantiate(shape, objectScale);
+        }
+    }
+
+    private PrimitiveType ConvertEnum(ShapeType inputShape)
+    {
+        switch (inputShape)
+        {
+            default:
+            case ShapeType.Cube:
+                return PrimitiveType.Cube;
+            case ShapeType.Sphere:
+                return PrimitiveType.Sphere;
+            case ShapeType.Cylinder:
+                return PrimitiveType.Cylinder;
+            case ShapeType.Capsule:
+                return PrimitiveType.Capsule;
+        }
+    }
+
+    enum ShapeType
+    {
+        Cube,
+        Sphere,
+        Cylinder,
+        Capsule
+    }
 }
---
+++ Assets/PrimitivesInstantiation/PrimitiveInstantiator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.PrimitivesInstantiation
+{
+    public static class PrimitiveInstantiator
+    {
+        public static GameObject Instantiate(PrimitiveType shape, Vector3 scale)
+        {
+            scale = Vector3.Max(scale, Vector3.one);
+            GameObject instance = GameObject.CreatePrimitive(shape);
+            instance.transform.localScale = scale;
+            return instance;
+        }
+    }
+}
