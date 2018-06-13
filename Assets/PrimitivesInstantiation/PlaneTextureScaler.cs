9adee055d70148ab5cce6bef25e8676a7bb44f47
Stacionarni-PC <p3ter.sik@gmail.com>
2018-06-13 09:06:17 +0200

    plane texture is tiling and collider working

---
+++ Assets/PrimitivesInstantiation/PlaneTextureScaler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneTextureScaler : MonoBehaviour
+{
+    private const int BASE_SCALE = 50;
+    Material mat;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        Vector3 scale = gameObject.transform.localScale;
+        Vector2 scale2D = new Vector2(scale.x / BASE_SCALE, scale.z / BASE_SCALE);
+        mat.SetTextureScale("_MainTex", scale2D);
+    }
+}
---
+++ Assets/PrimitivesInstantiation/TESTING_clicks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTING_clicks : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Ray rScreen = Camera.main.ScreenPointToRay(mousePos);
+            RaycastHit rh;
+            if (Physics.Raycast(rScreen, out rh))
+            {
+                print(rh.collider.gameObject.name);
+            }
+        }
+    }
+}
